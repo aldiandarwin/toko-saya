41934fcaaf7d0bcd96eccd07925251d57fda5f12
aldiandarwin <aldiandarwinputra@gmail.com>
2023-12-13 00:03:29 +0700

    add register page

---
+++ src/components/views/auth/RegisterView/index.tsx
@@ -0,0 +1,80 @@
+import { FormEvent, use } from 'react';
+import styles from './Register.module.scss';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const RegisterView = () => {
+    const { push } = useRouter();
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const form = event.target as HTMLFormElement;
+        const data = {
+            email: form.email.value,
+            fullname: form.fullname.value,
+            phone: form.phone.value,
+            password: form.password.value,
+        };
+
+        const result = await fetch('/api/user/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        if (result.status === 200) {
+            form.reset();
+            push('/auth/login');
+        } else {
+            console.log('error');
+        }
+    };
+    return (
+        <div className={styles.register}>
+            <h1 className={styles.register__title}>Register</h1>
+            <div className={styles.register__form}>
+                <form onSubmit={handleSubmit}>
+                    <div className={styles.register__form__item}>
+                        <label htmlFor='email'>email</label>
+                        <input name='email' id='email' type='text' className={styles.register__form__item__input} />
+                    </div>
+
+                    <div className={styles.register__form__item}>
+                        <label htmlFor='fullname'>Fullname</label>
+                        <input
+                            name='fullname'
+                            id='fullname'
+                            type='text'
+                            className={styles.register__form__item__input}
+                        />
+                    </div>
+
+                    <div className={styles.register__form__item}>
+                        <label htmlFor='phone'>Phone</label>
+                        <input name='phone' id='phone' type='text' className={styles.register__form__item__input} />
+                    </div>
+
+                    <div className={styles.register__form__item}>
+                        <label htmlFor='password'>Password</label>
+                        <input
+                            name='password'
+                            id='password'
+                            type='password'
+                            className={styles.register__form__item__input}
+                        />
+                    </div>
+
+                    <button type='submit' className={styles.register__button}>
+                        Register
+                    </button>
+                </form>
+                <p className={styles.register__link}>
+                    have an account?sign in <Link href='/auth/login'> here</Link>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterView;
---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,11 @@
+import RegisterView from '@/components/views/auth/RegisterView';
+
+const RegisterPage = () => {
+    return (
+        <>
+            <RegisterView />
+        </>
+    );
+};
+
+export default RegisterPage;
